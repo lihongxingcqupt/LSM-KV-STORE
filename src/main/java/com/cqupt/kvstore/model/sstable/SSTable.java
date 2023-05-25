de5664ab7a5cb28ca2384f3a575f52f595e6a542
lihongxing <lihongxing1019@163.com>
2023-05-25 22:19:35 +0800

    SSTable 元信息类、本类的创建，并且实现内存数据刷磁盘

--- src/main/java/com/cqupt/kvstore/model/sstable/SSTable.java
+++ src/main/java/com/cqupt/kvstore/model/sstable/SSTable.java
@@ -1,6 +1,7 @@
 package com.cqupt.kvstore.model.sstable;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.cqupt.kvstore.model.Position;
 import com.cqupt.kvstore.model.commond.Command;
 import com.cqupt.kvstore.model.commond.RmCommand;
@@ -20,11 +21,12 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * 维护SSTable的全部信息，另外的几个都是维护元信息的，这个是核心，可以通过方法构造SSTale，另外还可以从文件中加载SSTable
+ *
  * @Author lihongxing
  * @Date 2023/5/24 20:27
  */
 @Data
-public class SSTable implements Cloneable{
+public class SSTable implements Cloneable {
     private final Logger LOGGER = LoggerFactory.getLogger(SSTable.class);
 
     public static final String RW = "rw";
@@ -61,38 +63,40 @@ public class SSTable implements Cloneable{
 
     /**
      * 构造函数。初始化filePath和enablePartDataCompress，另外还有别的构造函数，起到别的作用
+     *
      * @param filePath
      * @param enablePartDataCompress
      */
-    private SSTable(String filePath,boolean enablePartDataCompress){
+    private SSTable(String filePath, boolean enablePartDataCompress) {
         this.filePath = filePath;
         this.enablePartDataCompress = enablePartDataCompress;
-        try{
-            this.tableFile = new RandomAccessFile(filePath,RW);
+        try {
+            this.tableFile = new RandomAccessFile(filePath, RW);
             tableFile.seek(0);
-        }catch (Throwable t){
+        } catch (Throwable t) {
             throw new RuntimeException(t);
         }
     }
 
     /**
      * 构造函数，用文件编号、数据分区大小、是否开启压缩、位于的层数来初始化sstable
+     *
      * @param fileNumber
      * @param partSize
      * @param enablePartDataCompress
      * @param level
      */
-    private SSTable(Long fileNumber,int partSize,boolean enablePartDataCompress,Integer level){
+    private SSTable(Long fileNumber, int partSize, boolean enablePartDataCompress, Integer level) {
         this.tableMetaInfo = new TableMetaInfo();
         this.tableMetaInfo.setNumber(fileNumber);
         this.tableMetaInfo.setPartSize(partSize);
         this.level = level;
-        this.filePath = FileUtils.buildSStableFilePath(fileNumber,level);
+        this.filePath = FileUtils.buildSStableFilePath(fileNumber, level);
         this.enablePartDataCompress = enablePartDataCompress;
-        try{
-            this.tableFile = new RandomAccessFile(filePath,RW);
+        try {
+            this.tableFile = new RandomAccessFile(filePath, RW);
             tableFile.seek(0);
-        }catch (Throwable t){
+        } catch (Throwable t) {
             throw new RuntimeException(t);
         }
         sparseIndex = new TreeMap<>();
@@ -100,6 +104,7 @@ public class SSTable implements Cloneable{
 
     /**
      * 将memtable构建成sstable
+     *
      * @param fileNumber
      * @param partSize
      * @param index
@@ -110,8 +115,8 @@ public class SSTable implements Cloneable{
     public static SSTable createFromIndex(Long fileNumber, int partSize,
                                           ConcurrentSkipListMap<String, Command> index,
                                           boolean enablePartDataCompress,
-                                          Integer level){
-        SSTable ssTable = new SSTable(fileNumber,partSize,enablePartDataCompress,level);
+                                          Integer level) {
+        SSTable ssTable = new SSTable(fileNumber, partSize, enablePartDataCompress, level);
         ssTable.initFromIndex(index);
         return ssTable;
     }
@@ -119,28 +124,28 @@ public class SSTable implements Cloneable{
     /**
      * 将跳表正式转成sstable
      */
-    private void initFromIndex(ConcurrentSkipListMap<String,Command> index){
+    private void initFromIndex(ConcurrentSkipListMap<String, Command> index) {
         try {
             JSONObject partData = new JSONObject(true);
             tableMetaInfo.setDataStart(tableFile.getFilePointer());
             for (Command command : index.values()) {
-                if(command instanceof SetCommand){
+                if (command instanceof SetCommand) {
                     SetCommand set = (SetCommand) command;
-                    partData.put(set.getKey(),set);
+                    partData.put(set.getKey(), set);
                 }
 
-                if(command instanceof RmCommand){
+                if (command instanceof RmCommand) {
                     RmCommand rm = (RmCommand) command;
-                    partData.put(rm.getKey(),rm);
+                    partData.put(rm.getKey(), rm);
                 }
 
                 // 达到分段数量，开始写入数据
-                if(partData.size() >= tableMetaInfo.getPartSize()){
+                if (partData.size() >= tableMetaInfo.getPartSize()) {
                     writeDataPart(partData);
                 }
             }
             // 遍历完以后如果有剩余数据，（没有达到分段而剩余的）
-            if(partData.size() > 0){
+            if (partData.size() > 0) {
                 writeDataPart(partData);
             }
             long dataPartLen = tableFile.getFilePointer() - tableMetaInfo.getDataStart();
@@ -157,7 +162,7 @@ public class SSTable implements Cloneable{
 
             // 该方法一调用就将上面的元信息写入tableFile指向的这个文件了，于是这个文件就有了数据、TreeMap的索引，元信息，并且key还是有序的
             tableMetaInfo.writeToFile(tableFile);
-        }catch (Throwable t){
+        } catch (Throwable t) {
             throw new RuntimeException(t);
         }
     }
@@ -165,7 +170,7 @@ public class SSTable implements Cloneable{
     private void writeDataPart(JSONObject partData) throws IOException {
         // 获取到字节流
         byte[] partDataBytes = partData.toJSONString().getBytes(StandardCharsets.UTF_8);
-        if(enablePartDataCompress){
+        if (enablePartDataCompress) {
             partDataBytes = Snappy.compress(partDataBytes);
         }
         // 获取文件当前的偏移量位置
@@ -183,7 +188,29 @@ public class SSTable implements Cloneable{
          * 查找的过程就变成了，打开一个sstable之后先二分查找key，完了通过红黑树Ologn的复杂度找到具体的位置，在这个位置不是直接拿到数据，因为是稀疏索引，需要再遍历
          * 为什么用TreeMap而不是hashMap
          */
-        firstKey.ifPresent(key -> sparseIndex.put(key,new Position(start,finalPartDataBytes.length)));
+        firstKey.ifPresent(key -> sparseIndex.put(key, new Position(start, finalPartDataBytes.length)));
         partData.clear();
+    }
+
+    /**
+     * 从文件中恢复sstable到内存
+     */
+    private void restoreFromFile() {
+        try {
+            // 先读取基本的元信息
+            TableMetaInfo tableMetaInfo = TableMetaInfo.readFromFile(tableFile);
+
+            // 读取稀疏索引
+            byte[] indexBytes = new byte[(int) tableMetaInfo.getIndexLen()];
+            tableFile.seek(tableMetaInfo.getIndexStart());
+            tableFile.read(indexBytes);
+            String indexStr = new String(indexBytes, StandardCharsets.UTF_8);
+            this.sparseIndex = JSONObject.parseObject(indexStr,
+                    new TypeReference<TreeMap<String, Position>>() {
+                    });
+//            tableMetaInfo.setNumber(FileUtils.);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 }
